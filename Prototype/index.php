221e8d0512a06c7352e5a6efc708e264fe32afce
patz100 <smul0003@flinders.edu.au>
2024-10-02 20:38:10 +0930

    v0.0.5

--- Prototype/index.php
+++ Prototype/index.php
@@ -5,9 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Developed_By" content="smul0003_basn0058_tami0009_will1941_beam0036_park0903">
-    <title>Document</title>
+    <title>Login_Page</title>
 </head>
 <body>
-    
+    <header>
+        <h1>
+        Company Name
+        </h1>
+        <ul>
+            <li>
+                Login Portal
+            </li>
+            <li>
+                Please login to continue to [Company Name] Dashboard
+            </li>
+        </ul>
+    </header>
+    <main>
+
+    </main>
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COMP2030-Software_Prototype_Group_2
 Software prototype for Group 2 Friday 9am
+
+**Developed By**
+*smul0003 basn0058 tami0009 will1941 beam0036 park0903*
