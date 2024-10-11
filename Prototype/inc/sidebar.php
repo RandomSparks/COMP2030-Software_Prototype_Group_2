886a685b4e1bfdff0da19078e6bc9f4d0ac5c9ed
RandomSparks <109492406+RandomSparks@users.noreply.github.com>
2024-10-12 00:17:45 +1030

    whitespace formatting tweaks

--- Prototype/inc/sidebar.php
+++ Prototype/inc/sidebar.php
@@ -1,76 +1,76 @@
-        <div id="div_sidebar">
-            <div id="div_logo">
-                <img src="../images/placeholder.jpg" alt="Logo">
-                <h1>Company Name</h1>
-            </div>
-            <ul id="ul_menu">
-                <li>
-                    <img src="../images/placeholder.jpg" alt="home">
-                    <a href="home.php">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="overview">
-                    <a href="overview.php">
-                        Overview
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="production">
-                    <a href="production.php">
-                        Production
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="performance">
-                    <a href="performance.php">
-                        Performance
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="alerts">
-                    <a href="alerts.php">
-                        Alerts
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="administation">
-                    <a href="admin.php">
-                        Administation
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="maintenance">
-                    <a href="maintenance.php">
-                        Maintenance
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="reporting">
-                    <a href="reporting.php">
-                        Reporting
-                    </a>
-                </li>
-            <div id="ul_support_settings">
-                <li>
-                    <img src="../images/placeholder.jpg" alt="settings">
-                    <a href="settings.php">
-                        Settings
-                    </a>
-                </li>
-                <li>
-                    <img src="../images/placeholder.jpg" alt="support">
-                    <a href="support.php">
-                        Support
-                    </a>
-                </li>
-            </div>
-            </ul>
-            <div id="div_logout">
-                <img src="../images/placeholder.jpg" alt="Logout">
-                <a href="../index.php">
-                    Logout
+<div id="div_sidebar">
+    <div id="div_logo">
+        <img src="../images/placeholder.jpg" alt="Logo">
+        <h1>Company Name</h1>
+    </div>
+    <ul id="ul_menu">
+        <li>
+            <img src="../images/placeholder.jpg" alt="home">
+            <a href="home.php">
+                Home
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="overview">
+            <a href="overview.php">
+                Overview
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="production">
+            <a href="production.php">
+                Production
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="performance">
+            <a href="performance.php">
+                Performance
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="alerts">
+            <a href="alerts.php">
+                Alerts
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="administation">
+            <a href="admin.php">
+                Administation
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="maintenance">
+            <a href="maintenance.php">
+                Maintenance
+            </a>
+        </li>
+        <li>
+            <img src="../images/placeholder.jpg" alt="reporting">
+            <a href="reporting.php">
+                Reporting
+            </a>
+        </li>
+        <div id="ul_support_settings">
+            <li>
+                <img src="../images/placeholder.jpg" alt="settings">
+                <a href="settings.php">
+                    Settings
                 </a>
-            </div>
+            </li>
+            <li>
+                <img src="../images/placeholder.jpg" alt="support">
+                <a href="support.php">
+                    Support
+                </a>
+            </li>
         </div>
+    </ul>
+    <div id="div_logout">
+        <img src="../images/placeholder.jpg" alt="Logout">
+        <a href="../index.php">
+            Logout
+        </a>
+    </div>
+</div>
