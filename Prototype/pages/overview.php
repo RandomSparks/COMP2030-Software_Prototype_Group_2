dcc3370d99bd75a547eaeff48523d404565bd497
patz100 <smul0003@flinders.edu.au>
2024-10-11 15:44:14 +1030

    Better menu pointer feedback

--- Prototype/pages/overview.php
+++ Prototype/pages/overview.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Developed_By" content="smul0003_basn0058_tami0009_will1941_beam0036_park0903">
-    <title>Dashboard/Home</title>
+    <title>Overview</title>
     <link rel="stylesheet" href="../styles/style.css">
     <script src="../scripts/script.js" defer></script>
 </head>
