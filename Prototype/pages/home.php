308a30d6d21a0ec943587ea77f3b40aa8acaadd0
patz100 <smul0003@flinders.edu.au>
2024-10-12 20:59:06 +1030

    forgor

--- Prototype/pages/home.php
+++ Prototype/pages/home.php
@@ -17,13 +17,13 @@
         require_once "../inc/header.php";
         ?>
         <div class="div_content">
-            <h1>
+            <h1 id="content_title">
                 Welcome to the [Company Name] Smart Manufacturing Dashboard, Hiroshi.
             </h1>
             <p>
                 This Dashboard will allow to view factory statuses, production information, performance metrics, and any factory alerts.
-                You can also edit accounts, assign jobs, and machines, manage job information and read reports from production operators.
-
+                You can also edit accounts, assign jobs, and machines, manage job information and read reports from production operators. <br>
+                <br>
                 You may also configure your personal settings, request support, or log out of your account.
             </p>
         </div>
