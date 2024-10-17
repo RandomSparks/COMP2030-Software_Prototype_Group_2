9553c79a0ce1f405d8eef6740b2147ea21ed07f6
patz100 <smul0003@flinders.edu.au>
2024-10-17 17:48:21 +1030

    Note content fixes

--- Prototype/pages/notes.php
+++ Prototype/pages/notes.php
@@ -71,7 +71,7 @@
             <form action='managenote.php' method='POST' id="form_createnote">
                 <input type="hidden" name="type" value="create">
                 <input type='text' name='name' placeholder='Note Name' maxlength="50" required>
-                <textarea name="content" id="textarea_createnote" placeholder="Note Content" maxlength="20"></textarea>
+                <textarea name="content" id="textarea_createnote" placeholder="Note Content" maxlength="100"></textarea>
                 <select name='machine_name' required>
                     <option value='CNC Machine'>CNC Machine</option>
                     <option value='3D Printer'>3D Printer</option>
