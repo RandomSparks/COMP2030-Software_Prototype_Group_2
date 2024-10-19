9d19886a33fdb12f251e001358f23667b67a950e
patz100 <smul0003@flinders.edu.au>
2024-10-19 23:07:30 +1030

    axed performance

--- Prototype/inc/sidebar.php
+++ Prototype/inc/sidebar.php
@@ -17,12 +17,6 @@
             </a>
         </li>
         <li>
-            <img src="../images/performanceIcon.png" alt="performance">
-            <a href="performance.php">
-                Performance
-            </a>
-        </li>
-        <li>
             <img src="../images/reportingIcon.png" alt="notes">
             <a href="notes.php">
                 Notes
@@ -34,20 +28,6 @@
                 Summary
             </a>
         </li>
-    <!--    <div id="ul_support_settings">
-            <li>
-                <img src="../images/settingsIcon.png" alt="settings">
-                <a href="settings.php">
-                    Settings
-                </a>
-            </li>
-            <li>
-                <img src="../images/supportIcon.png" alt="support">
-                <a href="support.php">
-                    Support
-                </a>
-            </li>
-        </div> -->
     </ul>
     <div id="div_logout">
         <img src="../images/logoutIcon.png" alt="Logout">
